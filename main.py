fe637ace9c72c3c47449614384b8934c26d93e85
SomeHu <2202304137@stu.jxufe.edu.cn>
2026-01-02 17:35:29 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from douban_fetch import fetch_collect_movies
+from notion_client import NotionClient
+
+if __name__ == "__main__":
+    douban_user = os.getenv("DOUBAN_USER_ID")
+    notion_token = os.getenv("NOTION_TOKEN")
+    database_id = os.getenv("NOTION_DATABASE_ID")
+
+    movies = fetch_collect_movies(douban_user, page_limit=1)
+
+    notion = NotionClient(notion_token, database_id)
+
+    for m in movies:
+        notion.create_movie(m)
