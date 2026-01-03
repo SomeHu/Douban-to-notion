d59090048078de707d8f98eba5ef2b603a34aa8c
SomeHu <2202304137@stu.jxufe.edu.cn>
2026-01-03 09:04:54 +0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from src.douban_fetch import fetch_all_movies
 from src.notion_client import NotionClient
 import os
 
+
 def main():
     douban_user = os.environ["DOUBAN_USER_ID"]
     notion = NotionClient(
@@ -12,8 +13,9 @@ def main():
     movies = fetch_all_movies(douban_user)
     print(f"Total movies: {len(movies)}")
 
-    for m in movies:
-        notion.create_movie(m)
+    for movie in movies:
+        notion.create_movie(movie)
+
 
 if __name__ == "__main__":
     main()
